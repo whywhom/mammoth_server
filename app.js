0d21c8e5cd0d959ca1247e6c4904e02ea598bc21
Andy Wu <t829530@spark.co.nz>
2023-07-14 12:37:01 +1200

    demo login

--- app.js
+++ app.js
@@ -1,44 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const router = require('./src/router')
+const middleware = require('./src/middleware')
 
-const index = require('./routes/index').default
-const users = require('./routes/users')
-
-// error handler
-onerror(app)
-
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
-
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+middleware(app)
+router(app)
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
 })
-
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
-
-module.exports = app
